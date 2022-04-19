ec9a0c2b2559401c8daa0626072a65a83a3c8420
Борга Инга <ednatasha2761@gmail.com>
2022-04-19 11:05:44 +0300

    Добавьте файлы проекта.

---
+++ Exam/ClassImg.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam
+{
+    public partial class Exam01
+    {
+        public string Img
+        {
+            get
+            {
+                if (Art != null)
+                {
+                    return Art;
+                }
+                else
+                {
+                    return "Image/picture.png";
+                }
+            }
+        }
+    }
+}
---
+++ Exam/PageShop.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Exam
+{
+    /// <summary>
+    /// Логика взаимодействия для PageShop.xaml
+    /// </summary>
+    public partial class PageShop : Page
+    {
+        List<Exam01> BooksCart = new List<Exam01>();
+
+        public PageShop()
+        {
+            InitializeComponent();
+            LVShop.ItemsSource = BaseConnect.Base.Exam01.ToList();
+        }
+
+        private void BTAdd_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedList = LVShop.SelectedItems;
+            TBCount.Text = "  " + selectedList.Count;
+            int totalCost = 0;
+            foreach (Exam01 n in selectedList)
+            {
+                totalCost += (int)n.Cost;
+                BooksCart.Add(n);
+            }
+            TBCost.Text = "  " + totalCost;
+        }
+
+        private void BTPer_Click(object sender, RoutedEventArgs e)
+        {
+            WindowCart cart = new WindowCart(BooksCart);
+            cart.ShowDialog();
+        }
+    }
+}
